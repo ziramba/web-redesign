b45ce782ba924f4fd732cb4820dfbc06171202e1
cnkunda <clirankunda@gmail.com>
2024-03-10 14:31:57 -0400

    Update seo component

--- contentful.config.js
+++ contentful.config.js
@@ -7,18 +7,18 @@ module.exports = {
     cpa_token: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN || '',
   },
   meta: {
-    title: 'Digital banking for the new generation | Colorful Coin',
-    description: `Enjoy premium banking services wherever you go: instant transfers and best exchange rates, exclusive offers and priority customer support. Apply online at ${url
+    title: 'Construction e-commerce made easy | Ziramba',
+    description: `We empower contractors by simplifying project sourcing, connecting with reliable suppliers, and offering hassle-free material financing. Get started at ${url
       .replace('https://', '')
       .replace('http://', '')}`,
     url,
     image:
-      'https://images.ctfassets.net/w8vf7dk7f259/4bucno7z1xAyVI5MOkU6Pu/ded83d0ec1eb732ae3a81ddab7a18877/fallback-image-03.jpg',
+      'https://res.cloudinary.com/ziramba/image/upload/v1639837701/ziramba/solution-embedded_ug1lwf.jpg',
   },
   icon: {
     light:
-      'https://images.ctfassets.net/w8vf7dk7f259/llZXwDCnl9NqdyuVvjn1n/d20cea90225e7f53dfbf8a18a46e972d/gocoin-icon-light.svg',
-    dark: 'https://images.ctfassets.net/w8vf7dk7f259/i9iu6GU6dFWQJJwJzwxCT/952cc3bab415e28f521c22933072a09c/gocoin-icon.svg',
+      'https://res.cloudinary.com/ziramba/image/upload/v1644795406/ziramba/android-chrome-512x512_zrkily.png',
+    dark: 'https://res.cloudinary.com/ziramba/image/upload/v1644795406/ziramba/android-chrome-512x512_zrkily.png',
     width: 66,
     height: 64,
   },
